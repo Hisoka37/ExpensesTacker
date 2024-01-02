e26adcce08830621478b4753bff98100660b0d53
Hisoka37 <walidkabou@gmail.com>
2024-01-02 18:13:00 +0100

    reupdate files - using ubuntu

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,18 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
+import '@hotwired/turbo-rails'
 import "controllers"
+import { Chart } from 'chart.js'
+
+console.log('hello world');
+document.addEventListener('turbo:load', () => {
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: JSON.parse(ctx.canvas.dataset.labels),
+      datasets: [{
+        data: JSON.parse(ctx.canvas.dataset.data),
+      }]
+    },
+    });
+  })
